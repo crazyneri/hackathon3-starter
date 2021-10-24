bf8a22ce779d5fc06ed4855d4e0a76b7d3fb4b82
crazyneri <flaskova.jana@gmail.com>
2021-10-24 15:11:36 +0200

    landing page w/login

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -1,19 +1,23 @@
 @extends('layouts\index')
 @section('main')
 
-<h1>Work Like Charm VetClinic</h1>
-
-<form action="/" method="get">
-<div class="form form-login">
-    <div class="login">
-        <label class="label-login" for="username">Username</form><br>
-        <input class="input input-login" type="text" name="username">
-    </div>
-    <div class="login">
-        <label class="label-login" for="username">Password</form><br>
-        <input class="input input-login" type="text" name="password">
-    </div>
-    <button class="btn btn-login">Log in</button>
+<div class="container-login">
+    <h1 class="heading-form">Work Like Charm VetClinic</h1>
+    
+    <form class="form-login" action="/" method="get">
+        <div class="container-form">
+            <div class="login">
+                <label class="label-login" for="username">Username</form>
+                <input class="input input-login" type="text" name="username">
+            </div>
+            <div class="login">
+                <label class="label-login" for="username">Password</form>
+                <input class="input input-login" type="text" name="password">
+            </div>
+            <div class="login">
+                <button class="btn btn-login">Log in</button>
+            </div>
+        </div>
+    </form>
 </div>
-</form>
 @endsection
